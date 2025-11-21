43e82ae928c7d821081ba408f800dc6a1e22d476
gitpriyanshu7 <meb25066@tezu.ac.in>
2025-11-21 22:19:04 +0530

    Add files via upload

---
+++ lab4/switch cas calculator.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main(){
+    int X;
+    printf("enter the number\n");
+    scanf("%d", &X);
+
+      switch(X){
+        case 1:
+        printf("monday\n");
+        break;
+        case 2:
+        printf("tuesday\n");
+        break;
+        case 3:
+        printf("wednesday\n");
+        break;
+        case 4:
+        printf("thrusday\n");
+        break;
+        case 5:
+        printf("friday\n");
+        break;
+        case 6:
+        printf("saturday\n");
+        break;
+        case 7:
+        printf("sunday\n");
+        break;
+        default:
+        printf("invalid day\n");
+
+      }
+    return 0;
+}
