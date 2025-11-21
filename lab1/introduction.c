a4ee2e20c44aefd82033f536952e2b89832aab23
gitpriyanshu7 <meb25066@tezu.ac.in>
2025-11-21 23:18:07 +0530

    Add files via upload

---
+++ lab1/introduction.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("My name is Priyanshu Phukon.\n");
+    printf("My branch is Mechanical Engineering.\n");
+    printf("My roll no is MEB25066.\n");
+
+return 0;
+}
